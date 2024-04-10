6e44c9d5db87f20f1c4a69942df9caca8897915f
Shubham Patil <shubhampatil871@gmail.com>
2024-04-10 15:32:46 +0530

    font changes

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -16,7 +16,7 @@ const Notification = ({ title, message }) => {
           className={`bg-blue-300 border border-blue-400 text-black px-2 py-1 m-1 rounded-xl relative`}
           role="alert"
         >
-          <p className="font-bold text-start">{title}</p>
+          <p className="font-bold text-start text-gray-800">{title}</p>
           <span className="absolute top-0 bottom-0 right-0 px-1 py-3 ">
             <img
               src={close}
@@ -27,7 +27,7 @@ const Notification = ({ title, message }) => {
               onClick={handleClose}
             />
           </span>
-          <span className="block sm:inline">{message}</span>
+          <span className="block sm:inline text-gray-700">{message}</span>
         </div>
       )}
     </>
