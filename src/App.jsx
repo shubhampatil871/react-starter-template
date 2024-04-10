d605ea3a40075380d28f6a1c155eb8a545835fa9
Shubham Patil <shubhampatil871@gmail.com>
2024-04-10 23:12:26 +0530

    added more styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from "react";
-import notifications from "./data/data";
+import notificationsArray from "./data/data";
 import Notification from "./components/Notification";
 
 const StarterTemplateInstructions = () => {
-  const [name, setName] = useState("");
+  const [notifications, setNotifications] = useState([]);
 
-  useEffect(() => {});
+  useEffect(() => {
+    setNotifications(notificationsArray);
+  }, []);
 
-  function handleChange(e) {
-    setName(e.target.value);
-  }
   console.log("This is rendered this much times :", name);
 
   return (
     <>
       <div className="flex items-start justify-center ">
-        <div className="px-3 py-2 my-20 bg-base-300 card w-[500px] text-wrap">
-          <div className="  text-center text-2xl font-semibold">
-            Notifications
-          </div>
-          {notifications.map((noti, index) => (
-            <Notification key={noti.id} title={noti.title} message={noti.message} />
+        <div className="  my-20 bg-base-300 card w-[500px] text-wrap rounded-none">
+          {notifications.map((noti) => (
+            <Notification
+              key={noti.id}
+              title={noti.title}
+              message={noti.message}
+            />
           ))}
         </div>
       </div>
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -12,23 +12,40 @@ const Notification = ({ title, message }) => {
   return (
     <>
       {show && (
-        <div
-          className={`bg-blue-300 border border-blue-400 text-black px-2 py-1 m-1 rounded-xl relative`}
-          role="alert"
-        >
-          <p className="font-bold text-start text-gray-800">{title}</p>
-          <span className="absolute top-0 bottom-0 right-0 px-1 py-3 ">
-            <img
-              src={close}
-              alt="Vite logo"
-              className="justify-end"
-              width={30}
-              height={50}
-              onClick={handleClose}
-            />
-          </span>
-          <span className="block sm:inline text-gray-700">{message}</span>
+        <div className="bg-gray-950 px-4 py-2 outline outline-1  ">
+          <p className="font-semibold text-slate-100 items-start">{title}</p>
+          <div className="flex items-center justify-between ">
+            <p className="block sm:inline text-slate-400">{message}</p>
+            <span className="">
+              <img
+                src={close}
+                alt="Vite logo"
+                className="cursor-pointer"
+                width={30}
+                height={50}
+                onClick={handleClose}
+              />
+            </span>
+          </div>
         </div>
+
+        // <div
+        //   className={`bg-blue-300 border border-blue-400 text-black px-2 py-1 m-1 rounded-xl relative`}
+        //   role="alert"
+        // >
+        //   <p className="font-bold text-start text-gray-800">{title}</p>
+        //   <span className="absolute top-0 bottom-0 right-0 px-1 py-3 ">
+        //     <img
+        //       src={close}
+        //       alt="Vite logo"
+        //       className="justify-end"
+        //       width={30}
+        //       height={50}
+        //       onClick={handleClose}
+        //     />
+        //   </span>
+        //   <span className="block sm:inline text-gray-700">{message}</span>
+        // </div>
       )}
     </>
   );
